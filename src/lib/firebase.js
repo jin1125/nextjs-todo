7df9dc9ab63f9b90abc90b744be3f5cfd29d559a
jin1125 <shogo.jinta@gmail.com>
2021-08-20 22:18:57 +0900

    test2

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,24 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBF2WkiMI3BzGDa6ixhNnDIyodRfR0i29Y",
-  authDomain: "nextjs-todo-b8a73.firebaseapp.com",
-  projectId: "nextjs-todo-b8a73",
-  storageBucket: "nextjs-todo-b8a73.appspot.com",
-  messagingSenderId: "1084375636163",
-  appId: "1:1084375636163:web:c0625c0849ad502ba22a76"
-};
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBF2WkiMI3BzGDa6ixhNnDIyodRfR0i29Y",
+//   authDomain: "nextjs-todo-b8a73.firebaseapp.com",
+//   projectId: "nextjs-todo-b8a73",
+//   storageBucket: "nextjs-todo-b8a73.appspot.com",
+//   messagingSenderId: "1084375636163",
+//   appId: "1:1084375636163:web:c0625c0849ad502ba22a76"
+// };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
