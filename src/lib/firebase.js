28298c15f10a7b69fcb345bbd307451ad11ea89e
jin1125 <shogo.jinta@gmail.com>
2021-08-23 14:08:16 +0900

    stage10

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,9 @@ import 'firebase/app'
 import "firebase/firestore";
 import 'firebase/auth'
 
-const firebaseConfig = {
+console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
+export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
